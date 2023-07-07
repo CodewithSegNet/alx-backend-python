25633a9701719b7526dfb56b936b281fd380a85e
segunUb18 <eeomo@yahoo.com>
2023-07-07 13:13:49 +0100

    :white_check_mark: task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+""" correct duck-typed annotations"""
 from typing import Sequence, Any, Union
+
+"""function def for safe first element"""
+
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if len(lst) > 0:
+        """ return 1st"""
         return lst[0]
+    """ return None"""
     else:
         return None
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,10 +3,14 @@
 return values with the appropriate types"""
 from typing import Iterable, Sequence, List, Tuple
 
+"""function’s parameters and return values with the appropriate types"""
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     result = []
+    """loop seq in 1st"""
     for seq in lst:
         length = len(seq)
         result.append((seq, length))
+        """return seq"""
     return result
