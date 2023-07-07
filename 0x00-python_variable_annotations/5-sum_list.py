95f2b2ef57118c79d47128b12e589f6e81a2096e
segunUb18 <eeomo@yahoo.com>
2023-07-07 04:14:02 +0100

    :white_check_mark: task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that takes a list of floats and return their sum"""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of list"""
+    return sum(input_list)
