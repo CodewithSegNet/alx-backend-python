98c5cccb451ab7470bea3dc3413de8f8cb2a2ef8
segunUb18 <eeomo@yahoo.com>
2023-07-07 12:03:42 +0100

    :ambulance: fix shebang for task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """A function that takes a list of floats and return their sum"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
     """return sum of list"""
-    sum_float = 0.0
-    for lists in input_list:
-        sum_float += lists
-    return sum_float
+    return sum(input_list)
