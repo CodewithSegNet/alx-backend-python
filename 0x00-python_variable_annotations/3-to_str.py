b93a489b08fbe9658dbc49aacd82c602700da745
segunUb18 <eeomo@yahoo.com>
2023-07-07 03:29:13 +0100

    :white_check_mark: task 2 & 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """A function that takes float as arg and return str repr of float"""
 
-def to_str(n: float) -> str:
 
+def to_str(n: float) -> str:
     """return str repr"""
+    
+    
     return str(n)
