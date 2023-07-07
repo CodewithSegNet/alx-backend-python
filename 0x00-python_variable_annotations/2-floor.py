a23b75ecbaad1619d4f6db64ead254c0fb23a273
segunUb18 <eeomo@yahoo.com>
2023-07-07 03:11:42 +0100

    :white_check_mark: task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""A function that takes floats as arg and returns the floor float"""
+
 import math
 
-"""A function that takes floats as arg and returns the floor float"""
 
 def floor(n: float) -> int:
     """returns a floor of n"""
+
     return math.floor(n)
