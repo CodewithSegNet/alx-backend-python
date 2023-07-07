37be53b64584023b93b7d8c9b7302e0e7af30dfd
segunUb18 <eeomo@yahoo.com>
2023-07-07 02:50:05 +0100

    :safety_vest: task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that takes two str and return a concatenated str"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """return concat of arg"""
+
+    return str1 + str2
