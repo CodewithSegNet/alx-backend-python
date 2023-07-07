c46af9843146d769f9119cddfaeffcb4ee5419ac
segunUb18 <eeomo@yahoo.com>
2023-07-07 12:27:20 +0100

    :safety_vest: task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A function that takes str and int or float as arg/
+return tuple"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return tuple of str and int or float"""
+    return k, v ** 2
+
