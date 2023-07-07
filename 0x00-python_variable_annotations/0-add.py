679dd6e23bb0d1f718844b55c7ffc5249cf78059
segunUb18 <eeomo@yahoo.com>
2023-07-07 02:34:38 +0100

    :white_check_mark: task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""type-annotated function add that takes floats as arg and returns their sum"""
-def add(a: float, b: float) -> float:
-    return a + b;
+"""Function that add two floats and return sum in float"""
 
+
+def add(a: float, b: float) -> float:
+    """function takes in two floats"""
+
+    return a + b
