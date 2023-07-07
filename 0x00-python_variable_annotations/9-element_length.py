a86156d3f71128e163c563d08e05244f58991833
segunUb18 <eeomo@yahoo.com>
2023-07-07 13:16:18 +0100

    :sa: fix task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,13 +4,13 @@ return values with the appropriate types"""
 from typing import Iterable, Sequence, List, Tuple
 
 """function’s parameters and return values with the appropriate types"""
-
-
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     result = []
     """loop seq in 1st"""
     for seq in lst:
+        """store seq in a variable length"""
         length = len(seq)
+        """ append seq"""
         result.append((seq, length))
         """return seq"""
     return result
