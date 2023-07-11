83a1635e1d626ba6295edf733bde067155b54cb8
omosWsl <eeomo@yahoo.com>
2023-07-11 13:47:27 +0100

    :safety_vest: test run task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''an async routine that takes in arg and \
+        return the list of all delays'''
+from typing import List
+import asyncio
+import random 
+
+wait = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    delay = await asyncio.gather(*[wait(max_delay) for _ in range(n)])
+    return sorted(delay)
