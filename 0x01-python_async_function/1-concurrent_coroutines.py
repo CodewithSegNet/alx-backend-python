15e2ecd8b7d69564d08b61fc862c3fa6e9f6ea5d
omosWsl <eeomo@yahoo.com>
2023-07-11 14:05:41 +0100

    :white_check_mark: documented function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,10 @@ import random
 
 wait = __import__('0-basic_async_syntax').wait_random
 
-
+'''def async wait_n function
+'''
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''store number of max_delay in delay
+    '''
     delay = await asyncio.gather(*[wait(max_delay) for _ in range(n)])
     return sorted(delay)
