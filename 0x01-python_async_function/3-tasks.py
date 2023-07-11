53f8a927c6c92afdd78dc0af1b3ba593932219d9
omosWsl <eeomo@yahoo.com>
2023-07-11 15:09:43 +0100

    :white_check_mark: task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,8 +8,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 '''function def
 '''
+
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     '''create an asynchronous task for wait_random
     '''
     return asyncio.create_task(wait_random(max_delay))
-
